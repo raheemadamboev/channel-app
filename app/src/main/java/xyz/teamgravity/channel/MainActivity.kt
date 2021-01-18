32f0d1a73a6a1e17dfb55e507af8a0b6a859c69a
Raheem <raheemideadev@gmail.com>
2021-01-18 17:41:47 +0500

    raheem

---
+++ app/src/main/java/xyz/teamgravity/channel/MainActivity.kt
@@ -0,0 +1,19 @@
+package xyz.teamgravity.channel
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import xyz.teamgravity.channel.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+    }
+}
